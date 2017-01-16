59e25dfe52482796ab38d4e60b9a3d8827e7bc65
Hashman Lin <coosos810609@gmail.com>
2017-01-16 20:35:10 +0800

    add test_for_一二三集各買一本 test case, get red light

--- tests/PotterShoppingCartTest.php
+++ tests/PotterShoppingCartTest.php
@@ -38,4 +38,22 @@ class PotterShoppingCartTest extends \PHPUnit\Framework\TestCase
         /** assert */
         $this->assertEquals($expect, $actual);
     }
+
+    public function test_for_一二三集各買一本()
+    {
+        /** arrange */
+        $first_season = 1;
+        $second_season = 1;
+        $third_season = 1;
+        $forth_season = 0;
+        $fifth_season = 0;
+        $expect = 270;
+
+        /** act */
+        $cart = new PotterShoppingCart($first_season, $second_season, $third_season, $forth_season, $fifth_season);
+        $actual = $cart->getPrice();
+
+        /** assert */
+        $this->assertEquals($expect, $actual);
+    }
 }
