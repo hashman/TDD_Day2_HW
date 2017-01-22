17df0273e4d97214a1384b32d99d9e36c0e8df21
Hashman Lin <coosos810609@gmail.com>
2017-01-22 13:39:10 +0800

    修正 test_for_第一集買了一本其他都沒有買 測試案例為新的結構， all red light

--- tests/PotterShoppingCartTest.php
+++ tests/PotterShoppingCartTest.php
@@ -6,15 +6,15 @@ class PotterShoppingCartTest extends \PHPUnit\Framework\TestCase
     public function test_for_第一集買了一本其他都沒有買()
     {
         /** arrange */
-        $first_season = 1;
-        $second_season = 0;
-        $third_season = 0;
-        $forth_season = 0;
-        $fifth_season = 0;
+        $cart = new PotterShoppingCart();
+        $cart->addBook(1, 1);
+        $cart->addBook(2, 0);
+        $cart->addBook(3, 0);
+        $cart->addBook(4, 0);
+        $cart->addBook(5, 0);
         $expect = 100;
 
         /** act */
-        $cart = new PotterShoppingCart($first_season, $second_season, $third_season, $forth_season, $fifth_season);
         $actual = $cart->getPrice();
 
         /** assert */
