3007781f6a8bc02d82674f78b0ac0ab9a4506313
Hashman Lin <coosos810609@gmail.com>
2017-01-22 13:45:43 +0800

    fixup 修正所有測試改為新架構， all green light

--- tests/PotterShoppingCartTest.php
+++ tests/PotterShoppingCartTest.php
@@ -24,15 +24,15 @@ class PotterShoppingCartTest extends \PHPUnit\Framework\TestCase
     public function test_for_第一集買了一本第二集也買了一本()
     {
         /** arrange */
-        $first_season = 1;
-        $second_season = 1;
-        $third_season = 0;
-        $forth_season = 0;
-        $fifth_season = 0;
+        $cart = new PotterShoppingCart();
+        $cart->addBook(1, 1);
+        $cart->addBook(2, 1);
+        $cart->addBook(3, 0);
+        $cart->addBook(4, 0);
+        $cart->addBook(5, 0);
         $expect = 190;
 
         /** act */
-        $cart = new PotterShoppingCart($first_season, $second_season, $third_season, $forth_season, $fifth_season);
         $actual = $cart->getPrice();
 
         /** assert */
@@ -42,15 +42,15 @@ class PotterShoppingCartTest extends \PHPUnit\Framework\TestCase
     public function test_for_一二三集各買一本()
     {
         /** arrange */
-        $first_season = 1;
-        $second_season = 1;
-        $third_season = 1;
-        $forth_season = 0;
-        $fifth_season = 0;
+        $cart = new PotterShoppingCart();
+        $cart->addBook(1, 1);
+        $cart->addBook(2, 1);
+        $cart->addBook(3, 1);
+        $cart->addBook(4, 0);
+        $cart->addBook(5, 0);
         $expect = 270;
 
         /** act */
-        $cart = new PotterShoppingCart($first_season, $second_season, $third_season, $forth_season, $fifth_season);
         $actual = $cart->getPrice();
 
         /** assert */
@@ -60,15 +60,15 @@ class PotterShoppingCartTest extends \PHPUnit\Framework\TestCase
     public function test_for_一二三四集各買一本()
     {
         /** arrange */
-        $first_season = 1;
-        $second_season = 1;
-        $third_season = 1;
-        $forth_season = 1;
-        $fifth_season = 0;
+        $cart = new PotterShoppingCart();
+        $cart->addBook(1, 1);
+        $cart->addBook(2, 1);
+        $cart->addBook(3, 1);
+        $cart->addBook(4, 1);
+        $cart->addBook(5, 0);
         $expect = 320;
 
         /** act */
-        $cart = new PotterShoppingCart($first_season, $second_season, $third_season, $forth_season, $fifth_season);
         $actual = $cart->getPrice();
 
         /** assert */
@@ -78,15 +78,15 @@ class PotterShoppingCartTest extends \PHPUnit\Framework\TestCase
     public function test_for_買一整套的()
     {
         /** arrange */
-        $first_season = 1;
-        $second_season = 1;
-        $third_season = 1;
-        $forth_season = 1;
-        $fifth_season = 1;
+        $cart = new PotterShoppingCart();
+        $cart->addBook(1, 1);
+        $cart->addBook(2, 1);
+        $cart->addBook(3, 1);
+        $cart->addBook(4, 1);
+        $cart->addBook(5, 1);
         $expect = 375;
 
         /** act */
-        $cart = new PotterShoppingCart($first_season, $second_season, $third_season, $forth_season, $fifth_season);
         $actual = $cart->getPrice();
 
         /** assert */
@@ -96,15 +96,15 @@ class PotterShoppingCartTest extends \PHPUnit\Framework\TestCase
     public function test_for_一二集各買一本第三級買兩本()
     {
         /** arrange */
-        $first_season = 1;
-        $second_season = 1;
-        $third_season = 2;
-        $forth_season = 0;
-        $fifth_season = 0;
+        $cart = new PotterShoppingCart();
+        $cart->addBook(1, 1);
+        $cart->addBook(2, 1);
+        $cart->addBook(3, 2);
+        $cart->addBook(4, 0);
+        $cart->addBook(5, 0);
         $expect = 370;
 
         /** act */
-        $cart = new PotterShoppingCart($first_season, $second_season, $third_season, $forth_season, $fifth_season);
         $actual = $cart->getPrice();
 
         /** assert */
@@ -114,15 +114,15 @@ class PotterShoppingCartTest extends \PHPUnit\Framework\TestCase
     public function test_for_第一集一本二三集各買兩本()
     {
         /** arrange */
-        $first_season = 1;
-        $second_season = 2;
-        $third_season = 2;
-        $forth_season = 0;
-        $fifth_season = 0;
+        $cart = new PotterShoppingCart();
+        $cart->addBook(1, 1);
+        $cart->addBook(2, 2);
+        $cart->addBook(3, 2);
+        $cart->addBook(4, 0);
+        $cart->addBook(5, 0);
         $expect = 460;
 
         /** act */
-        $cart = new PotterShoppingCart($first_season, $second_season, $third_season, $forth_season, $fifth_season);
         $actual = $cart->getPrice();
 
         /** assert */
